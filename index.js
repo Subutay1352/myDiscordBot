1f482231f20434d39d4c210fd9e79a311c7dca7c
Subutay1352 <90280598+Subutay1352@users.noreply.github.com>
2023-09-13 19:32:25 +0000

    getAPIDetails

--- index.js
+++ index.js
@@ -1,57 +1,44 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000,() => {
+app.listen(3000, () => {
   console.log("Project is running!");
 });
 
-app.get("/",(req,res) => {
+app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers,
-	],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
 });
 
-client.once("ready",async message => {
-  console.log("Bot is online!");
-});
 
 client.on('messageCreate', message => {
-if (message.content === '!ping') {
-message.reply('Pong!');
-}
-});
-
-client.on('messageCreate', message => {
-if (message.content === '!Bivalvan') {
-message.reply('Usta üst koridor!');
-}
-});
-
-client.on('messageCreate', message => {
-if (message.content === '!status') {
-message.reply('açıgım baba!');
-}
-});
-
-client.on('messageCreate', message => {
-if (message.content === '!Efeniko') {
-message.reply('Viego Master!');
-}
-});
-
-client.on('messageCreate', message => {
-if (message.content === '!JacksonTahiroğlu') {
-message.reply('Mal gibi bişi aq!');
-}
+  switch (message.content) {
+    case '!status':
+      message.reply('Active!');
+      break;
+    case '!Efeniko':
+      message.reply('Viego Master!');
+      break;
+    case '!JacksonTahiroğlu':
+      message.reply('Mmmh?');
+      break;
+    case '!Bivalvan':
+      message.reply('Experienced top laner!');
+      break;
+    default:
+      message.reply('Server is on!');
+      break;
+  }
 });
 
 const mySecret = process.env['token'];
